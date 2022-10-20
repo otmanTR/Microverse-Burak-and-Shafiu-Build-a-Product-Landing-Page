359bc9ee6a533fb0fdd0780542ada0785bd12ce9
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2022-10-20 16:01:22 +0000

    second commit

---
+++ pagelanding/src/PageLanding/PageLanding.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './PageLanding.css'
+
+const PageLanding = () => {
+  return (
+    <div>
+        <div className='navigation-bar'>
+                <ul>
+                    <div className='left-navbar'>
+                        <span className='left'>Original Trombones</span>
+                    </div>
+                </ul>
+                <ul>
+                    <li>Feature</li>
+                    <li>How it works</li>
+                    <li>Pricing</li>
+                </ul>
+        </div>
+       
+      
+            
+    </div>
+  )
+}
+
+export default PageLanding
